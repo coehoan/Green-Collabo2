5fa5a19fce0fe658749b5c8493390eed7c01cde0
coehoan <coehoan@naver.com>
2022-04-04 17:27:10 +0900

    [팀장] Initial commit

---
+++ src/main/java/site/metacoding/collabo2/Collabo2Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.collabo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collabo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Collabo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/collabo2/Collabo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.collabo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collabo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
